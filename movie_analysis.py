f30221205c42a92bf5ede599adcae95e861563c2
mr <rajarammaitreyi7@gmail.com>
2020-03-08 13:14:27 -0400

    first commit

---
+++ movie_analysis.py
@@ -0,0 +1,21 @@
+import unirest
+import pandas as pd
+
+
+def get_actor_race(actor_name):
+
+def get_actors(movie_name):
+
+
+def get_movie_titles():
+
+
+def main():
+    get_movie_titles() #from imdb from 1999-2019, nominees + winners
+    for item in dataframe_movie_titles:
+        get_actors() ## append list of actors to dataframe
+        get_actor_race()
+
+
+if __name__ == '__main__':
+    main()
