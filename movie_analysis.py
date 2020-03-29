007def5daf16719cf4c4695c9b0544acb3980f7d
mr <rajarammaitreyi7@gmail.com>
2020-03-29 03:09:47 -0400

    dataframes + api key in config

--- movie_analysis.py
+++ movie_analysis.py
@@ -1,21 +1,55 @@
-import unirest
 import pandas as pd
+import config
+import requests
+
+#TODO- actor race
 
 
-def get_actor_race(actor_name):
+##globals
+all_casts = [] #list of list of tuples (actor name, cast no) for each film
+all_budgets = []
 
-def get_actors(movie_name):
+all_actors = [] #list of names
+# FULL CAST/GENDERS FOR EACH: 2 - guy, 1 - girl, ignore 0s (unfortunately makes for small margin of error)
+all_actor_genders = [] #list of genders (added in order)
 
+def get_movie_ids(filename):
+    df = pd.read_csv(filename, usecols = ["Const"])
+    return df
 
-def get_movie_titles():
+def get_budget(movie_id):  ##finish this
+    call ='https://api.themoviedb.org/3/movie/' + str(movie_id) + '?api_key=' + config.MY_KEY + '&language=en-US'
+    r = requests.get(call)
+    budget = r.json().get('budget')
+    all_budgets.append(budget)
 
+def get_cast(movie_id):
+    call = 'https://api.themoviedb.org/3/movie/' + str(movie_id) + '/credits?api_key=' + config.MY_KEY
+    r = requests.get(call)
+    cast = r.json().get('cast')
+    # iterate through cast
+    cast_for_movie = []
+    for item in cast:
+        cast_for_movie.append((item.get('name'), item.get('cast_id')))  # adding cast[(actor_name, rank_of_appearance)]
+        all_actors.append(item.get('name'))
+        all_actor_genders.append(item.get('gender'))#add actor name and gender to list
+    all_casts.append(cast_for_movie)
 
 def main():
-    get_movie_titles() #from imdb from 1999-2019, nominees + winners
-    for item in dataframe_movie_titles:
-        get_actors() ## append list of actors to dataframe
-        get_actor_race()
+    movies = get_movie_ids("BestPictureAcademyAward.csv")
 
+    for Const in movies.itertuples(): #for each movie title
+        movie_id = (Const[1])
+        get_cast(movie_id)
+        get_budget(movie_id)
+    movies['Cast'] = all_casts
+    movies['Budget'] = all_budgets
+    actors = pd.DataFrame(all_actors, columns=['Name'])
+    actors['Gender'] = all_actor_genders
+
+    #print final dataframes
+    print(movies)
+    print(actors)
 
 if __name__ == '__main__':
     main()
