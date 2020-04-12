dccff4f30470902b246b3328125f4d8cb000a3be
Amy Bangad <asb250@scarletmail.rutgers.edu>
2020-04-12 13:12:33 -0400

    changed visualization code to methods

--- FrequencyGraphs.py
+++ FrequencyGraphs.py
@@ -3,32 +3,37 @@ import altair as alt
 import altair_viewer as av
 import pandas as pd
 
-df = pd.read_csv('data/FilmData2.csv')
-bechdel_pass = df[df['Bechdel Pass'] == True] # all the films that pass the Bechdel test
-bechdel_fail = df[df['Bechdel Pass'] == False] # all the films that fail the Bechdel test
+# divides full movie data into two data frames, one for films that pass the bechdel test, one for those that fail
+# films with no bechdel scores are not included
+def split_bechdel(df):
+    bechdel_pass = df[df['Bechdel Pass'] == True] # all the films that pass the Bechdel test
+    bechdel_fail = df[df['Bechdel Pass'] == False] # all the films that fail the Bechdel test
+    return bechdel_pass, bechdel_fail
 
-pass_genres = []
-for row in bechdel_pass['Genres']:
-    genres = row.split(", ")
-    for word in genres:
-        pass_genres.append(word)
+# creates freqency graphs for genres of films that pass the bechdel test and those that don't
+def bechdel_genre_graphs(bechdel_pass, bechdel_fail):
+    pass_genres = []
+    for row in bechdel_pass['Genres']:
+        genres = row.split(", ")
+        for word in genres:
+            pass_genres.append(word)
 
-pd.Series(pass_genres).value_counts().plot(kind='bar', colormap = 'plasma', title = 'Genres of Films that Pass the Bechdel Test')
-plt.figure()
+    pd.Series(pass_genres).value_counts().plot(kind='bar', colormap = 'plasma', title = 'Genres of Films that Pass the Bechdel Test')
+    plt.figure()
 
 
-fail_genres = []
-for row in bechdel_fail['Genres']:
-    genres = row.split(", ")
-    for word in genres:
-        fail_genres.append(word)
-pd.Series(fail_genres).value_counts().plot(kind='bar',colormap = 'viridis', title = 'Genres of Films that Fail the Bechdel Test')
-plt.figure()
-plt.show()
+    fail_genres = []
+    for row in bechdel_fail['Genres']:
+        genres = row.split(", ")
+        for word in genres:
+            fail_genres.append(word)
+    pd.Series(fail_genres).value_counts().plot(kind='bar',colormap = 'viridis', title = 'Genres of Films that Fail the Bechdel Test')
+    plt.figure()
+    plt.show()
 
 # how to make altair grouped bar chart???
-# group pass v fail foro each genre
-
+# group pass v fail for each genre
+'''
 df2 = pd.DataFrame(columns = ['Genre', 'Test'])
 for i in range(len(pass_genres)):
     df2.loc[i] = [pass_genres[i], 'Pass']
@@ -39,4 +44,4 @@ chart = alt.Chart(df2).mark_bar().encode(
     x='Test:Q',
     y='count(Genre):Q'
 )
-chart.show()
+chart.show() '''
